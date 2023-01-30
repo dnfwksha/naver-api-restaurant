729b01abff8aaea2fb88bbbb0d9b0d45bcc706c6
Juhwan <dnfwksha@gmail.com>
2023-01-30 22:01:26 +0900

    페이지 작성 완료

--- src/main/java/com/example/demo/db/MemoryDbEntity.java
+++ src/main/java/com/example/demo/db/MemoryDbEntity.java
@@ -4,9 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+//@Entity
 public class MemoryDbEntity {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     protected Integer index;
 }
